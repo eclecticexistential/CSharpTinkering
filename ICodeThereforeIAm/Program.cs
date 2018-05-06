55e13bc2b7de89dfe2bfcabd63006a21cee10390
eclecticexistential <allofitbra@gmail.com>
2018-05-06 16:15:31 -0700

    Calling a function in Main()

--- ICodeThereforeIAm/Program.cs
+++ ICodeThereforeIAm/Program.cs
@@ -10,12 +10,17 @@ namespace ICodeThereforeIAm
     {
         static void Main(string[] args)
         {
-            string op = "WOrds yo!";
-            for (int i = 0; i < op.Length; i++)
-            {
-                Console.WriteLine("Hello {0} : {1}!", i, op[i]);
-            }
+            string[] myArgs = Environment.GetCommandLineArgs();
+            Console.WriteLine(string.Join(", ", myArgs));
+            SayHello();
             Console.ReadLine();
+        }
+        private static void SayHello()
+        {
+            string name = "";
+            Console.Write("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Greetings {0}!", name);
         }
     }
 }
