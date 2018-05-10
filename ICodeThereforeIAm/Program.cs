79ddeb65589eb3183e413cb30f1a616567a5343b
eclecticexistential <allofitbra@gmail.com>
2018-05-10 00:30:14 -0700

    Tinkering with getters setters

--- ICodeThereforeIAm/Program.cs
+++ ICodeThereforeIAm/Program.cs
@@ -6,17 +6,35 @@ namespace ICodeThereforeIAm
     {
         static void Main(string[] args)
         {
-            
+            Animals cat = new Animals();
+            cat.SetName("Whiskers");
+            cat.Sound = "Meow";
+            Console.WriteLine("The cat is named {0} and says {1}", cat.GetName(), cat.Sound);
+            cat.Owner = "Brutus";
+            Console.WriteLine("{0} owner is {1}", cat.GetName(), cat.Owner);
+            Console.WriteLine("{0} shelter id is {1}", cat.GetName(), cat.idNum);
+            Animals bear = new Animals("George", "Rrrrr");
+            Console.WriteLine("# of animals : {0}", Animals.NumOfAnimal);
+
+            GetLessons();
+            Console.ReadLine();
+        }
+
+        public static void GetLessons()
+        {
             Animal fox = new Animal()
             {
                 name = "Red",
-                sound = "Raaw"
+                sound = "Raaw",
             };
 
-            fox.name = "Red";
-            fox.sound = "Raaw";
-
             fox.MakeSound();
+
+            Animal tiger = new Animal();
+            tiger.name = "Strips";
+            tiger.sound = "Roar";
+
+            tiger.MakeSound();
 
             Console.WriteLine("# of Animals : {0}",
                 Animal.GetNumAnimals());
@@ -27,8 +45,6 @@ namespace ICodeThereforeIAm
                 ShapeMath.GetArea("triangle", 7, 4));
             Console.WriteLine("Area of Circle : {0}",
                 ShapeMath.GetArea("circle", 7, 4));
-
-            Console.ReadLine();
         }
     }
 }
