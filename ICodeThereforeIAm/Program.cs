851147436d4c5fc120a79e2cf3a8d74bacec45c1
eclecticexistential <allofitbra@gmail.com>
2018-05-11 16:02:12 -0700

    Created dice rolling game where number of rolls are counted until perfect 20 is achieved. Player is given option to keep rolling or quit to receive the message "Goodbye".

--- ICodeThereforeIAm/Program.cs
+++ ICodeThereforeIAm/Program.cs
@@ -6,45 +6,36 @@ namespace ICodeThereforeIAm
     {
         static void Main(string[] args)
         {
-            Animals cat = new Animals();
-            cat.SetName("Whiskers");
-            cat.Sound = "Meow";
-            Console.WriteLine("The cat is named {0} and says {1}", cat.GetName(), cat.Sound);
-            cat.Owner = "Brutus";
-            Console.WriteLine("{0} owner is {1}", cat.GetName(), cat.Owner);
-            Console.WriteLine("{0} shelter id is {1}", cat.GetName(), cat.idNum);
-            Animals bear = new Animals("George", "Rrrrr");
-            Console.WriteLine("# of animals : {0}", Animals.NumOfAnimal);
-
-            GetLessons();
-            Console.ReadLine();
-        }
-
-        public static void GetLessons()
-        {
-            Animal fox = new Animal()
+            RollAgain();
+            Console.WriteLine("Roll again?");
+            string ans = Console.ReadLine();
+            while (ans.ToLower() == "yes")
             {
-                name = "Red",
-                sound = "Raaw",
+                RollAgain();
+                Console.WriteLine("Roll again?");
+                ans = Console.ReadLine();
             };
 
-            fox.MakeSound();
-
-            Animal tiger = new Animal();
-            tiger.name = "Strips";
-            tiger.sound = "Roar";
-
-            tiger.MakeSound();
-
-            Console.WriteLine("# of Animals : {0}",
-                Animal.GetNumAnimals());
-
-            Console.WriteLine("Area of Rectangle : {0}",
-                ShapeMath.GetArea("rectangle", 5, 6));
-            Console.WriteLine("Area of Triangle : {0}",
-                ShapeMath.GetArea("triangle", 7, 4));
-            Console.WriteLine("Area of Circle : {0}",
-                ShapeMath.GetArea("circle", 7, 4));
+            Console.WriteLine("Goodbye!");
+        }
+        public static void RollAgain()
+        {
+            Random rnd = new Random();
+            int chance = rnd.Next(0, 21);
+            int counter = 0;
+            while (chance != 20)
+            {
+                if (chance <= 10)
+                {
+                    counter++;
+                }
+                else if (chance < 20)
+                {
+                    counter++;
+                }
+                chance = rnd.Next(0, 21);
+            }
+            Console.WriteLine($"Congrats You Rolled a Critical after {counter} rolls!");
         }
     }
 }
